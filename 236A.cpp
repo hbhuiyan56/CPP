b39ca2cc123a5332fd664d0151013d96c971e650
hbhuiyan56 <152174536+hbhuiyan56@users.noreply.github.com>
2025-09-19 20:03:48 +0600

    j

---
+++ 236A.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string username;
+    int n;
+    int count =0;
+    cin>>username;
+    int l = username.length();
+    for (int i = 0 ; i < l ; i++)
+    {
+        for (int j = i +1; j < l; j++)
+        {
+            if (username[i] == username[j]){
+                count ++;
+                break;
+            }
+        }
+    }
+    // cout << count;
+    n = l - count;
+    if (n % 2 == 0){
+        cout<<"CHAT WITH HER!";
+    }
+    else{
+        cout<<"IGNORE HIM!";
+    }
+    return 0;
+}
